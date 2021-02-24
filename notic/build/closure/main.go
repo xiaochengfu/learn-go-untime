14e466f12f1bcc9ece49a82077ac7c876ddb637f
侯鹏 <houpeng@doushen.com>
2021-02-24 19:38:03 +0800

    feat: for内的闭包函数

--- notic/build/closure/main.go
+++ notic/build/closure/main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+func loop() {
+	var addressI *int
+	for i := 0; i < 2; i++ {
+		fmt.Println(i, &i)
+		addressI = &i
+
+	}
+	fmt.Println(addressI, *addressI)
+}
+
 func test() []func() {
 	var funs []func()
 	for i := 0; i < 2; i++ {
-		i := i //i重写赋值没有搞清楚
+		fmt.Println("before:", &i, i)
+		//i := i //这里要重新赋值，修改掉i的内存地址，f()每次调用才是不同的值
+		fmt.Println("end:", &i, i)
 		funs = append(funs, func() {
 			//匿名函数的优越性在于可以直接使用函数内的变量，不必申明
 			println(&i, i)
@@ -15,12 +27,10 @@ func test() []func() {
 }
 
 func main() {
+	loop()
+	fmt.Println("-----------")
 	funs := test()
-	fmt.Println(funs)
 	for _, f := range funs {
 		f()
 	}
 }
-
-//0xc000018090 0
-//0xc000018098 1
